7e4332a67415e1e9b074e95a70752b94570688e4
bent0008 <benturnock00@gmail.com>
2022-11-20 22:01:27 -0500

    Create DigitalCash.cs
    Added the template

---
+++ DigitalCash.cs
@@ -0,0 +1,30 @@
+// Ben Turnock
+// Program for Digital Cash
+
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+
+namespace NewNamespace
+{
+	class NewClass
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadKey();
+		}
+	}
+}
+
