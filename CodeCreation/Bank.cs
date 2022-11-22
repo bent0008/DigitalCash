fce1bc66c5b24aeebbe5b5da5167ff3ce1806fe0
bent0008 <benturnock00@gmail.com>
2022-11-22 00:02:17 -0500

    program.cs, bob, and bank
    updated program.cs
    added bob.cs
    added bank.cs and moved things from program.cs over

---
+++ CodeCreation/Bank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCreation
+{
+    internal class Bank
+    {
+        private bool moneyCheated = false;
+        private bool serialNumCheated = false;
+
+        static int ChooseMoneyOrder(int n)
+        {
+            Random rand = new();
+            int chosenOrder = rand.Next(n);
+
+            return chosenOrder;
+        }
+        public bool CheckMoney(int n, List<string> mlist)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (mlist[0] != mlist[i])
+                {
+                    Console.WriteLine("Money amounts do not match!");
+                    moneyCheated = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Money amounts match. Pass!");
+                }
+            }
+
+            if (moneyCheated == true)
+            {
+                Console.WriteLine("You cheated the money amount!");
+            }
+            return moneyCheated;
+        }
+
+        public bool CheckSerialNumber(int n, List<string> snlist)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //Console.WriteLine("i = " + snlist[i] + " j = " + snlist[j]);
+                    if (i != j)
+                    {
+                        Console.WriteLine("i = " + snlist[i] + " j = " + snlist[j]);
+                        if (snlist[i] == snlist[j])
+                        {
+                            Console.WriteLine("You cheated the serial number!");
+                            serialNumCheated = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            return serialNumCheated;
+        }
+
+        public void BankSignature(int n, List<string> mlist , List<string> snlist)
+        {
+            string bankSign = "3531961060";
+
+            // amount > serial number > bank signature
+            string[] moneyOrder = {bankSign};
+
+        }
+    }
+}
--- CodeCreation/Program.cs
+++ CodeCreation/Program.cs
@@ -15,56 +15,80 @@ using System.Linq;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices;
+using CodeCreation;
 
 namespace DigitalCash
 {
-    class NewClass
+    class Alice
     {
         static void Main(string[] args)
         {
-            InputMoney();
-            string serialNumber = GenerateSerialNumber();
-            Console.WriteLine(serialNumber);
+            string leftIdentity, rightIdentity;
+
+            Console.Write("How many money orders? ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            var moneyList = new List<string>();
+            var serialNumList = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string moneyAmount = InputMoney();
+                moneyList.Add(moneyAmount);
+
+                string serialNumber = CreateSerialNumber();
+                serialNumList.Add(serialNumber);
+
+                //CreateIdentification(out leftIdentity, out rightIdentity);
+
+                //Console.WriteLine("Left: " + leftIdentity);
+                //Console.WriteLine("Right: " + rightIdentity);
+            }
+
+            Bank Banker = new();
+            Banker.CheckMoney(n, moneyList);
+            Banker.CheckSerialNumber(n, serialNumList);
 
             Console.ReadKey();
         }
 
 
         // input amount of money in transaction
-        static int InputMoney()
+        static string InputMoney()
         {
             Console.Write("Amount: $");
-            int moneyAmount = Int32.Parse(Console.ReadLine());
+            string moneyAmount = Console.ReadLine();
             return moneyAmount;
         }
 
 
         // generate unique serial ID number as string
-        static string GenerateSerialNumber()
+        static string CreateSerialNumber()
         {
-            int length = 10;
-            const string numberList = "0123456789";
-            StringBuilder serialNum = new();
-            Random rand = new();
-
-            while (0 < length--)
-            {
-                serialNum.Append(numberList[rand.Next(numberList.Length)]);
-            }
             Console.Write("Serial Number: ");
+            string serialNum = Console.ReadLine();
 
-            return serialNum.ToString();
+            return serialNum;
+
+            //int length = 10;
+            //Random rand = new();
+            //string serialNum = "";
+
+            //while(0 < length--)
+            //{
+            //    serialNum += rand.Next(length);
+            //}
         }
 
 
         // identity strings with identity of customer (remains hidden unless customer tries to use the ecash illicitly more than once)
-        static string GenerateIdentification()
+        private static void GenerateIdentification(out string leftIdentity, out string rightIdentity)
         {
-            Console.Write("First Name: ")
+            Console.Write("First Name: ");
             string fname = Console.ReadLine();
-            Console.Write("Last Name: ")
+            Console.Write("Last Name: ");
             string lname = Console.ReadLine();
-            Console.Write("Account Number: ")
+            Console.Write("Account Number: ");
             string accountNumber = Console.ReadLine();
 
             string identity = fname + lname + accountNumber;
@@ -73,22 +97,20 @@ namespace DigitalCash
             int separateNum = 2;
 
             int chars = length / separateNum;
+            leftIdentity = "";
+            rightIdentity = "";
 
             for (int i = 0; i < chars; i++)
             {
-                string leftIdentity = chars[i];
+                leftIdentity += identity[i];
             }
 
-            for (int j = length; j > chars; j--)
+            for (int j = chars; j < length; j++)
             {
-                string rightIdentity = chars[i];
+                rightIdentity += identity[j];
             }
-
-
-
         }
 
-        // bank's signature (before customer uses ecash)
 
 
     }
